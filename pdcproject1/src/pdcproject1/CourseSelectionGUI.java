138b97a349d913d5a595920440f5ad0c5867870b
Jeromejosephh <jeromejoseph311@gmail.com>
2023-10-30 19:10:09 +1300

    Update GUI

--- pdcproject1/src/pdcproject1/CourseSelectionGUI.java
+++ pdcproject1/src/pdcproject1/CourseSelectionGUI.java
@@ -4,6 +4,7 @@ package pdcproject1;
  *
  * @author jeromejoseph
  */
+//import statements
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -16,7 +17,7 @@ import java.io.IOException;
 import java.util.Random;
 import java.util.Set;
 
-public class CourseSelectionGUI {
+public class CourseSelectionGUI { //Course Selection GUI class
 
     private JFrame frame;
     private JPanel studentInfoPanel;
@@ -35,13 +36,26 @@ public class CourseSelectionGUI {
     private JButton viewCoursesButton;
     private JButton viewSelectedCoursesButton;
 
+    private boolean isValidFirstName(String firstName) { //input validation for first name
+        return firstName.matches("[a-zA-Z]+");
+    }
 
+    private boolean isValidLastName(String lastName) { //input validation for last name
+        return lastName.matches("[a-zA-Z]+");
+    }
+
+    private boolean isValidEmail(String email) { //input validation for email
+        return email.matches("^[A-Za-z0-9+_.-]+@(.+)$");
+    }
+
+    //constructor
     public CourseSelectionGUI() {
         frame = new JFrame("Course Selection System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
-        frame.setLayout(new GridLayout(2, 2));
+        frame.setLayout(new GridLayout(2, 4));
 
+        //create and initialize components
         createStudentInfoPanel();
         createCourseSelectionPanel();
         createCourseListPanel();
@@ -50,112 +64,129 @@ public class CourseSelectionGUI {
         frame.setVisible(true);
     }
 
+    //create Student Information Panel
     private void createStudentInfoPanel() {
         studentInfoPanel = new JPanel();
-        studentInfoPanel.setLayout(new GridLayout(4, 2));
+        studentInfoPanel.setLayout(new GridLayout(8, 4));
 
-        JLabel firstNameLabel = new JLabel("First Name:");
-        firstNameLabel.setFont(new Font("Arial", Font.PLAIN, 14)); 
+        //add labels, text fields, and submit button
+        JLabel firstNameLabel = new JLabel("First Name:", SwingConstants.CENTER);
+        firstNameLabel.setFont(new Font("Arial", Font.PLAIN, 14)); //set font
         firstNameField = new JTextField(20);
-        firstNameField.setFont(new Font("Arial", Font.PLAIN, 14));
+        firstNameField.setFont(new Font("Arial", Font.PLAIN, 14)); //set font
 
-        JLabel lastNameLabel = new JLabel("Last Name:");
-        lastNameLabel.setFont(new Font("Arial", Font.PLAIN, 14));
+        JLabel lastNameLabel = new JLabel("Last Name:", SwingConstants.CENTER);
+        lastNameLabel.setFont(new Font("Arial", Font.PLAIN, 14)); //set font
         lastNameField = new JTextField(20);
-        lastNameField.setFont(new Font("Arial", Font.PLAIN, 14));
+        lastNameField.setFont(new Font("Arial", Font.PLAIN, 14)); //set font
 
-        JLabel emailLabel = new JLabel("Email:");
-        emailLabel.setFont(new Font("Arial", Font.PLAIN, 14)); 
+        JLabel emailLabel = new JLabel("Email:", SwingConstants.CENTER);
+        emailLabel.setFont(new Font("Arial", Font.PLAIN, 14)); //set font
         emailField = new JTextField(20);
-        emailField.setFont(new Font("Arial", Font.PLAIN, 14));
+        emailField.setFont(new Font("Arial", Font.PLAIN, 14)); //set font
 
         submitButton = new JButton("Submit");
-        submitButton.setFont(new Font("Arial", Font.BOLD, 16));
-        submitButton.setBackground(Color.GREEN);
-        submitButton.setForeground(Color.BLACK); 
+        submitButton.setFont(new Font("Arial", Font.BOLD, 16)); //set font and make it bold
+        submitButton.setForeground(Color.BLACK); //set button text color
 
+        //add components to the panel
         studentInfoPanel.add(firstNameLabel);
         studentInfoPanel.add(firstNameField);
         studentInfoPanel.add(lastNameLabel);
         studentInfoPanel.add(lastNameField);
         studentInfoPanel.add(emailLabel);
         studentInfoPanel.add(emailField);
-        studentInfoPanel.add(new JLabel());
+        studentInfoPanel.add(new JLabel()); //empty label for spacing
         studentInfoPanel.add(submitButton);
 
         frame.add(studentInfoPanel);
     }
 
-
+    //create Course Selection Panel
     private void createCourseSelectionPanel() {
         courseSelectionPanel = new JPanel();
         courseSelectionPanel.setLayout(new BorderLayout());
 
+        //add course selection components
         frame.add(courseSelectionPanel);
     }
 
-
+    //create Course List Panel
     private void createCourseListPanel() {
         courseListPanel = new JPanel();
         courseListPanel.setLayout(new BorderLayout());
 
+        //add a scrollable text area
         courseListTextArea = new JTextArea();
         courseListTextArea.setEditable(false);
         JScrollPane scrollPane = new JScrollPane(courseListTextArea);
         courseListPanel.add(scrollPane);
 
         viewCoursesButton = new JButton("View Courses");
-        viewCoursesButton.setFont(new Font("Arial", Font.BOLD, 16)); 
-        viewCoursesButton.setBackground(Color.BLUE); 
-        viewCoursesButton.setForeground(Color.BLACK);
+        viewCoursesButton.setFont(new Font("Arial", Font.BOLD, 16)); //set font and make it bold
+        viewCoursesButton.setForeground(Color.BLACK); //set button text color
         courseListPanel.add(viewCoursesButton, BorderLayout.SOUTH);
 
         frame.add(courseListPanel);
     }
 
-   
+    //create Selected Courses Panel
     private void createSelectedCoursesPanel() {
         selectedCoursesPanel = new JPanel();
         selectedCoursesPanel.setLayout(new BorderLayout());
 
-      
+        //add a scrollable text area for selected courses
         selectedCoursesTextArea = new JTextArea();
         selectedCoursesTextArea.setEditable(false);
         JScrollPane scrollPane = new JScrollPane(selectedCoursesTextArea);
         selectedCoursesPanel.add(scrollPane);
 
         viewSelectedCoursesButton = new JButton("View Selected Courses");
-        viewSelectedCoursesButton.setFont(new Font("Arial", Font.BOLD, 16)); 
-        viewSelectedCoursesButton.setBackground(Color.BLUE);
-        viewSelectedCoursesButton.setForeground(Color.BLACK);
+        viewSelectedCoursesButton.setFont(new Font("Arial", Font.BOLD, 16)); //set font and make it bold
+        viewSelectedCoursesButton.setForeground(Color.BLACK); //set button text color
         selectedCoursesPanel.add(viewSelectedCoursesButton, BorderLayout.SOUTH);
 
         frame.add(selectedCoursesPanel);
     }
 
-
+    //action listeners for buttons
     private void addListeners() {
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //get user's input from text fields
                 String firstName = firstNameField.getText();
                 String lastName = lastNameField.getText();
                 String email = emailField.getText();
 
-                if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty()) {
-                    JOptionPane.showMessageDialog(frame, "Please fill in all fields", "Error", JOptionPane.ERROR_MESSAGE);
-                } else {
-                    int studentID = generateRandomStudentID(); 
-                    Student student = new Student(studentID, firstName + " " + lastName, email);
-
-                    JOptionPane.showMessageDialog(frame, "Student information saved successfully", "Success", JOptionPane.INFORMATION_MESSAGE);
+                //input validation
+                if (!isValidFirstName(firstName)) {
+                    JOptionPane.showMessageDialog(frame, "Invalid first name. Please enter letters only.", "Error", JOptionPane.ERROR_MESSAGE);
+                    return; //stop processing further if validation fails
                 }
+
+                if (!isValidLastName(lastName)) {
+                    JOptionPane.showMessageDialog(frame, "Invalid last name. Please enter letters only.", "Error", JOptionPane.ERROR_MESSAGE);
+                    return; //stop processing further if validation fails
+                }
+
+                if (!isValidEmail(email)) {
+                    JOptionPane.showMessageDialog(frame, "Invalid email address. Please enter a valid email.", "Error", JOptionPane.ERROR_MESSAGE);
+                    return; //stop processing further if validation fails
+                }
+
+                //if all input is valid, proceed to create the Student object
+                int studentID = generateRandomStudentID();
+                Student student = new Student(studentID, firstName + " " + lastName, email);
+
+                JOptionPane.showMessageDialog(frame, "Student information saved successfully", "Success", JOptionPane.INFORMATION_MESSAGE);
             }
         });
 
         viewCoursesButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //Read and display course information
                 try {
                     BufferedReader reader = new BufferedReader(new FileReader("resources/courses.txt"));
                     StringBuilder courseInfo = new StringBuilder();
@@ -165,10 +196,12 @@ public class CourseSelectionGUI {
                         courseInfo.append(line).append("\n");
                     }
 
+                    courseListTextArea.setCaretPosition(0);
                     courseListTextArea.setText(courseInfo.toString());
 
                     reader.close();
                 } catch (IOException ex) {
+                    //handle any exceptions
                     ex.printStackTrace();
                 }
             }
@@ -177,11 +210,14 @@ public class CourseSelectionGUI {
         viewSelectedCoursesButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //handle view selected courses button
+                //you can add code here to load and display selected courses from a file.
                 loadAndDisplaySelectedCourses();
             }
         });
     }
 
+    //load and display selected courses
     private void loadAndDisplaySelectedCourses() {
         try {
             BufferedReader reader = new BufferedReader(new FileReader("resources/selected_courses.txt"));
@@ -196,11 +232,12 @@ public class CourseSelectionGUI {
 
             reader.close();
         } catch (IOException ex) {
-
+            //handle any exceptions
             ex.printStackTrace();
         }
     }
 
+    //save selected courses to file
     public void saveSelectedCourses(Set<String> selectedCourses) {
         try {
             PrintWriter writer = new PrintWriter(new FileWriter("resources/selected_courses.txt"));
@@ -211,20 +248,22 @@ public class CourseSelectionGUI {
 
             writer.close();
         } catch (IOException e) {
-
+            //handle exceptions
             e.printStackTrace();
         }
     }
 
+    //main method
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             CourseSelectionGUI gui = new CourseSelectionGUI();
-            gui.addListeners();
+            gui.addListeners(); //add action listeners after GUI setup
         });
     }
 
+    //generate random student ID
     private int generateRandomStudentID() {
         Random random = new Random();
-        return 100000 + random.nextInt(900000);
+        return 100000 + random.nextInt(900000); //generates 6-digit random ID
     }
 }
